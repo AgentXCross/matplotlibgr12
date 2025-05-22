304f1537a35d1f8a220d3fffbd1de5b734e08a35
Michael Liu <liumichaeler@gmail.com>
2025-05-22 11:36:44 -0400

    Second histogram for % Workforce in STEM added

--- histogram.py
+++ histogram.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-gdp_per_worker = [ #data for histogram
+gdp_per_worker = [ #data set #1 for histogram
     139054.84, 149734.53, 109288.54, 125959.58, 105513.48,
     108618.67, 161430.35, 130445.52, 151336.56, 155698.46,
     115580.11, 123660.61, 125564.36, 105291.61, 139067.20,
@@ -15,16 +15,44 @@ gdp_per_worker = [ #data for histogram
     133649.44, 175084.70, 119117.41, 117060.37, 119515.27
 ]
 
-plt.hist(gdp_per_worker, bins = 7, edgecolor = 'black', color = 'skyblue') #7 histogram bins
+workforce_in_stem = [ #data set #2 for histogram
+    10.40, 9.90, 9.50, 10.70, 8.50, 7.40, 12.70, 14.60, 12.90, 10.80,
+    9.50, 10.60, 10.20, 9.20, 11.90, 10.10, 9.10, 11.50, 9.60, 9.20,
+    6.70, 10.60, 15.20, 16.10, 11.60, 12.70, 7.60, 10.60, 10.70, 8.40,
+    7.80, 7.60, 11.00, 8.50, 14.40, 13.50, 11.20, 11.80, 11.50, 10.40,
+    8.90, 10.60, 8.80, 12.40, 7.30, 12.10, 8.40, 11.10, 7.50, 9.90,
+    10.50, 9.70, 10.50, 10.60, 12.10, 14.00, 14.90, 9.20, 10.70, 7.80
+]
 
-plt.title( 
-    'Nominal GDP Per Worker in Canada and the United States of America',
-    fontdict = {'fontsize': 13, 'fontweight': 'bold', 'family': 'Georgia'}
-    )
-plt.xlabel('GDP Per Worker ($USD)', fontsize = 12, fontweight = 'medium', family = 'Georgia') #X and Y axis
-plt.ylabel('Number of States/Provinces', fontsize = 12, fontweight = 'medium', family = 'Georgia')
+data_set = input("Which Data Set would you like to view?\n\t1. GDP Per Worker of all U.S. States and Canadian Provinces\n\t2. % Workforce in STEM of all U.S. States and Canadian Provinces\n")
+match data_set:
+    case "1":
+        plt.hist(gdp_per_worker, bins = 7, edgecolor = 'darkcyan', color = 'darkturquoise') #7 histogram bins for GDP per worker
 
-plt.xticks(fontname = "Georgia", fontsize = 10) #Adjust tick font and size
-plt.yticks(fontname = "Georgia", fontsize = 10)
+        plt.title(  #Title name and font
+            'Nominal GDP Per Worker in Canadian Provinces and U.S. States',
+            fontdict = {'fontsize': 13, 'fontweight': 'bold', 'family': 'Georgia'}
+            )
+        plt.xlabel('GDP Per Worker ($USD)', fontsize = 12, fontweight = 'medium', family = 'Georgia') #X and Y axis
+        plt.ylabel('# of States/Provinces', fontsize = 12, fontweight = 'medium', family = 'Georgia')
 
-plt.show()
+        plt.xticks(fontname = 'Georgia', fontsize = 10) #Adjust tick font and size
+        plt.yticks(fontname = 'Georgia', fontsize = 10)
+
+        plt.show()
+    case "2":
+        plt.hist(workforce_in_stem, bins = 7, edgecolor = 'mediumvioletred', color = 'deeppink') #7 histogram bins for % Workforce in STEM
+
+        plt.title(
+            '% Workforce in STEM of U.S. States and Canadian Provinces',
+            fontdict = {'fontsize': 13, 'fontweight': 'bold', 'family': 'Georgia'}
+            )
+        plt.xlabel('% Workforce in STEM', fontsize = 12, fontweight = 'medium', family = 'Georgia')
+        plt.ylabel('# of States/Provinces', fontsize = 12, fontweight = 'medium', family = 'Georgia')
+
+        plt.xticks(fontname = 'Georgia', fontsize = 10)
+        plt.yticks(fontname = 'Georgia', fontsize = 10)
+
+        plt.show()
+    case _:
+        print("Please enter either 1 or 2\n")
