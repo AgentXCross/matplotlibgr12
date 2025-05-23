9095d5340b7fc411d38668b9fa808235d5bb3b2e
Michael Liu <liumichaeler@gmail.com>
2025-05-22 22:10:34 -0400

    MVP for three graphs

--- desmos.py
+++ desmos.py
@@ -11,4 +11,27 @@ y_cos = np.cos(x_trig)
 y_ln = np.log(x_ln)
 
 #Create a plot for the graphs
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize = (10, 6))
+
+#enter axes at (0,0), MatPlotLib Usually creates a box, we are removing the top and right side of box and moving the left and bottom to (0,0) to create 2 axes
+ax.spines['left'].set_position('zero')   #y-axis at x=0
+ax.spines['bottom'].set_position('zero') #x-axis at y=0
+ax.spines['right'].set_color('none')     #hide right border, used to be apart of the box
+ax.spines['top'].set_color('none')       #hide top border
+
+#Set line thickness
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2)
+
+#Add ticks for the x and y axis
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+#Plot functions on the ax
+ax.plot(x_trig, y_sin, color = 'royalblue', linewidth = 3, alpha = 0.8)         #y = sin(x), adjusted linewidth and opacity
+ax.plot(x_trig, y_cos, color = 'fuchsia', linewidth = 3, alpha = 0.8)           #y = cos(x)
+ax.plot(x_ln, y_ln, color = 'palevioletred', linewidth = 3, alpha = 0.8)        #y = ln(x)
+
+plt.show()
+
+
